85a441dbdb32db0cdd9b76e978757a6cc9e78e0e
Varun Athreya <varun.athreya@contentstack.com>
2024-11-24 21:02:07 +0530

    added function schema generation

---
+++ api/src/tools/index.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { convert } from 'html-to-text';
+
+export default {
+  scrape_tool: {
+    id: 'scrape_tool',
+    name: 'scrape_tool',
+    description: 'This tool scrapes data from the given website',
+    type: 'function',
+    func: async (url: string) => {
+      const response = await axios.get(url);
+      return {
+        data: convert(response.data),
+      };
+    },
+  },
+  ats_score: {
+    id: 'ats_score',
+    name: 'ats_score',
+    description:
+      'This tool ATS scores the given resume based on Job Description',
+    type: 'function',
+    func: async (resume: string, jobDescription: string) => {
+      const response = await axios.post('https://api.ats.com/score', {
+        resume,
+        jobDescription,
+      });
+      return response.data;
+    },
+  },
+};
