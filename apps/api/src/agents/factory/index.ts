a2d3374c43d05df6d54114da696b6b1f6821b13c
Varun Athreya <varun.athreya@contentstack.com>
2025-02-02 11:47:45 +0530

    moved code inside apps

--- apps/api/src/agents/factory/index.ts
+++ apps/api/src/agents/factory/index.ts
@@ -1,13 +1,10 @@
 import { AgentProvider, IAgent } from '../../models/Agent.model';
 import { AgentBase } from './base';
-import GeminiAgent from './gemini';
 import OpenAIAgent from './openai';
 
 class AgentFactory {
   static createAgent(agent: IAgent): AgentBase {
     switch (agent.provider) {
-      case AgentProvider.GEMINI:
-        return new GeminiAgent(agent);
       case AgentProvider.OPENAI:
         return new OpenAIAgent(agent);
       default:
